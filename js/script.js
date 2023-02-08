ba51b6513fd336c2567a0aaaa4c5914c57a9bd39
andikaakskyt <andikaakskyt67@gmail.com>
2023-02-08 18:25:14 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elementTujuan = $(tujuan);
+
+
+	// pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elementTujuan.offset().top - 50
+	}, 1000, 'easeInOutExpo');
+
+
+	e.preventDefault();
+
+});
+
+
+// parallax
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+});
+
+
+
+
